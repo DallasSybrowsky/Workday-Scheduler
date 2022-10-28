40c320f4271c97a9ba153d308cdd465d0345b2cb
[Dallas Sybrowsky] <dallas.sybrowsky@gmail.com>
2022-10-27 22:01:22 -0700

    Update to script.js to move closer towards full functionality

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,8 @@
 // Date block code (done)
 const currentDate = moment().format("dddd, MMMM Do, YYYY");
+const timeInstant = moment();
 $("#currentDay").text(currentDate);
+scheduleUpdate();
 
 // Object to contain arrays for tasks (done)
 var tasks = {
@@ -15,39 +17,57 @@ var tasks = {
   17: [],
 };
 
-// Create blocks for inputting tasks 
-let createTask = function (taskItem, timeDiv) {
+// Create blocks for inputting tasks
+function createTask(taskItem, timeDiv) {
   let taskDiv = timeDiv.find(".task");
   let taskP = $("<p>").addClass("toDo").text(taskItem);
   taskDiv.html(taskP);
-};
+}
 
 // Save tasks to local storage (done)
-// Start of code block
-var setTasks = function () {
+function setTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+}
 $(".saveBtn").on("click", function () {
   const text = $(this).siblings(".task-data").val();
   const time = $(this).parent().attr("id");
   localStorage.setItem(time, text);
+  //save text area data into local storage
+  // remove text area
+  // load local storage data into div
 });
-// End of code block
 
 // Display tasks in their appropriate place
+$(".task").on("click", function(event) {
+    console.log(event.target);
+    var textArea = document.createElement('textarea');
+    textArea.classList.add("event-details");
+    console.log("textarea: " + textArea);
+    event.target.append(textArea);
+
+})
 
 // Set hour blocks to appropriate color (done)
-let currentHour = moment().hour();
-$(".task-data").each(function () {
-  var taskHour = parseInt($(this).attr("id"));
-  if (taskHour < currentHour) {
-    $(this).removeClass(["present", "future"]).addClass("past");
-  } else if (taskHour == currentHour) {
-    $(this).removeClass(["past", "future"]).addClass("present");
-  } else {
-    $(this).removeClass(["past", "present"]).addClass("future");
-  }
-});
+function scheduleUpdate() {
+//   let currentHour = moment().hour();
+let currentHour = 11
+  console.log("ScheduleUpdate", currentHour);
+  $(".task-data").each(function () {
+    var taskHour = parseInt($(this).attr("id"));
+    if (taskHour < currentHour) {
+      $(this).removeClass(["present", "future"]).addClass("past");
+    } else if (taskHour == currentHour) {
+      $(this).removeClass(["past", "future"]).addClass("present");
+    } else {
+      $(this).removeClass(["past", "present"]).addClass("future");
+    }
+  });
+}
+
+// Update hour blocks based on current time
+var timeUpdate = setInterval(function () {
+    scheduleUpdate()
+}, 60000);
 
 // Clear Schedule button (done)
 var clearCalendar = document.querySelector(".reset-btn");
