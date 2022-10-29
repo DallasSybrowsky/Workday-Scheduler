9367a57bb5c787bb61d65a778f07e248981c0c05
[Dallas Sybrowsky] <dallas.sybrowsky@gmail.com>
2022-10-28 17:23:48 -0700

    Updated script.js with functional code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,53 +4,17 @@ const timeInstant = moment();
 $("#currentDay").text(currentDate);
 scheduleUpdate();
 
-// Object to contain arrays for tasks (done)
-var tasks = {
-  9: [],
-  10: [],
-  11: [],
-  12: [],
-  13: [],
-  14: [],
-  15: [],
-  16: [],
-  17: [],
-};
 
-// Create blocks for inputting tasks
-function createTask(taskItem, timeDiv) {
-  let taskDiv = timeDiv.find(".task");
-  let taskP = $("<p>").addClass("toDo").text(taskItem);
-  taskDiv.html(taskP);
-}
-
-// Save tasks to local storage (done)
-function setTasks() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
 $(".saveBtn").on("click", function () {
-  const text = $(this).siblings(".task-data").val();
+  const text = $(this).siblings(".task").val();
   const time = $(this).parent().attr("id");
   localStorage.setItem(time, text);
-  //save text area data into local storage
-  // remove text area
-  // load local storage data into div
+  
 });
-
-// Display tasks in their appropriate place
-$(".task").on("click", function(event) {
-    console.log(event.target);
-    var textArea = document.createElement('textarea');
-    textArea.classList.add("event-details");
-    console.log("textarea: " + textArea);
-    event.target.append(textArea);
-
-})
 
 // Set hour blocks to appropriate color (done)
 function scheduleUpdate() {
-//   let currentHour = moment().hour();
-let currentHour = 11
+  let currentHour = moment().hour();
   console.log("ScheduleUpdate", currentHour);
   $(".task-data").each(function () {
     var taskHour = parseInt($(this).attr("id"));
@@ -73,4 +37,21 @@ var timeUpdate = setInterval(function () {
 var clearCalendar = document.querySelector(".reset-btn");
 clearCalendar.addEventListener("click", function () {
   localStorage.clear();
+  location.reload();
+});
+
+function renderData() {
+  $(".9-").val(localStorage.getItem("9"));
+  $(".10-").val(localStorage.getItem("10"));
+  $(".11-").val(localStorage.getItem("11"));
+  $(".12-").val(localStorage.getItem("12"));
+  $(".13-").val(localStorage.getItem("1"));
+  $(".14-").val(localStorage.getItem("2"));
+  $(".15-").val(localStorage.getItem("3"));
+  $(".16-").val(localStorage.getItem("4"));
+  $(".17-").val(localStorage.getItem("5"));
+}
+
+$(document).ready(function () {
+  renderData();
 });
